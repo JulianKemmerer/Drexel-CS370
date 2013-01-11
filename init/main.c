c164c0d169c080971387b49c12f95561e04908d3
Timothy Hahn <timyhahn@gmail.com>
2013-01-11 10:53:52 -0500

    Added the parameter check for 'printme' and the subsequent printk statement. TODO: Figure out why KERN_DEBUG level messages won't show  up in dmesg

--- init/main.c
+++ init/main.c
@@ -151,6 +151,9 @@ static unsigned int max_cpus = NR_CPUS;
 unsigned int reset_devices;
 EXPORT_SYMBOL(reset_devices);
 
+unsigned int print_me;
+EXPORT_SYMBOL(print_me);
+
 /*
  * Setup routine for controlling SMP activation
  *
@@ -184,6 +187,14 @@ static int __init set_reset_devices(char *str)
 }
 
 __setup("reset_devices", set_reset_devices);
+
+static int __init set_print_me(char *str)
+{
+    print_me = 1;
+    return 1;
+}
+
+__setup("printme", set_print_me);
 
 static char * argv_init[MAX_INIT_ARGS+2] = { "init", NULL, };
 char * envp_init[MAX_INIT_ENVS+2] = { "HOME=/", "TERM=linux", NULL, };
@@ -626,6 +637,10 @@ asmlinkage void __init start_kernel(void)
 	if (late_time_init)
 		late_time_init();
 	calibrate_delay();
+        if(print_me)
+        {
+            printk("HELLO AND WELCOME TO THE WORLD OF TOMORROW!\n");
+        }
 	pidmap_init();
 	pgtable_cache_init();
 	prio_tree_init();
